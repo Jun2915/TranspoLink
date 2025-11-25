39b65593682a05de513134d5a565f2636a291131
Jun <l.jun0310@gmail.com>
2025-11-25 16:46:09 +0800

    Program.Cs

--- TranspoLink/Program.cs
+++ TranspoLink/Program.cs
@@ -2,20 +2,69 @@ global using TranspoLink.Models;
 global using TranspoLink;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Services
 builder.Services.AddControllersWithViews();
+
+//Database
 builder.Services.AddSqlServer<DB>($@"
     Data Source=(LocalDB)\MSSQLLocalDB;
     AttachDbFilename={builder.Environment.ContentRootPath}\DB.mdf;
 ");
+
+//Register custom services
 builder.Services.AddScoped<Helper>();
-builder.Services.AddAuthentication().AddCookie();
+
+//Authentication & Cookies
+builder.Services.AddAuthentication("Cookies").AddCookie("Cookies", options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.LogoutPath = "/Account/Logout";
+    options.AccessDeniedPath = "/Account/AccessDenied";
+    options.ExpireTimeSpan = TimeSpan.FromDays(30);
+    options.SlidingExpiration = true;
+});
+
+builder.Services.AddAuthorization();
+
+//Session & Http Context
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddSession();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
+
+//Exception Handling
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+//Localization
 app.UseRequestLocalization("en-MY");
+
+// Session before authentication
 app.UseSession();
-app.MapDefaultControllerRoute();
+
+// Authentication & Authorization
+app.UseAuthentication();
+app.UseAuthorization();
+
+//Routes
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
